171b73baf689b5bce5abb4ae728758c5620e668f
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-26 17:41:15 +0900

    [Bronze V] Title: 고양이, Time: 68 ms, Memory: 11368 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		//고양이 출력
+		System.out.println( "\\    /\\" );
+		System.out.println( " )  ( ')" );
+		System.out.println( "(  /  )" );
+		System.out.println( " \\(__)|" );
+	}
+}
