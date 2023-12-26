be79995d90defdab285de10e3e30f242bdb7f88c
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-26 18:06:57 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11224 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+
+		// 이스케이프 시퀀스(Escape Sequence)
+		// \자체를 출력하고 싶다면 \\로 백슬래시를 두 번 입력
+		System.out.println("|\\_/| \n|q p|   /} \n( 0 )\"\"\"\\ \n|\"^\"`    | \n||_/=\\\\__|");
+		
+	}
+}
