71c2934747327071f91309a482356e35d0898c11
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-26 17:48:27 +0900

    Update 고양이.java

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,5 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
+				
+        	//이스케이프 시퀀스(Escape Sequence)
+        	// \자체를 출력하고 싶다면 \\로 백슬래시를 두 번 입력
+
 		//고양이 출력
 		System.out.println( "\\    /\\" );
 		System.out.println( " )  ( ')" );
