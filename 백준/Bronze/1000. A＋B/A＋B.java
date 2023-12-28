fcf76be63231f1487ebba40c74f9310aab98391f
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-28 22:36:25 +0900

    Update A＋B.java

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		int A = scanner.nextInt();
-		int B = scanner.nextInt();
-		
-		scanner.close();
-		
+		Scanner scan = new Scanner(System.in);
+
+		//A와 B값을 입력 받는다!
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+
+		//scanner 닫기!
+		scan.close();
+
+		//A+B 값 출력!
 		System.out.println(A+B);
 	}
 }
