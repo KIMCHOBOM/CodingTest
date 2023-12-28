14c0cc5b3d85a8c18a4ff43fbe1b351c7e93d5ff
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-28 22:41:31 +0900

    Update A／B.java

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-        
-		double A = scanner.nextDouble();
-		double B = scanner.nextDouble();
-		
-		scanner.close();
-		
+		Scanner scan = new Scanner(System.in);
+
+		//A와 B값 입력받기(0 < A, B < 10)
+		double A = scan.nextDouble();
+		double B = scan.nextDouble();
+
+		//scanner 닫기!
+		scan.close();
+
+		// A/B값 출력(출력값의 절대오차 또는 상대오차가 10^(-9) 이하여야 함!)
 		System.out.println(A/B);
 	}
 }
