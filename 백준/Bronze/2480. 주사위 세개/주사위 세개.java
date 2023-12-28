29c1aab67536c4976f3340f10d8ea23ced78eca3
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-29 01:00:19 +0900

    Update 주사위 세개.java

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -17,18 +17,18 @@ public class Main {
 		}
 		
 		//A와 B가 같거나 A와 C가 같은 경우
-        else if(A == B || A == C) {
-        	System.out.print(1000 + A*100);
-        }
+	        else if(A == B || A == C) {
+	        	System.out.print(1000 + A*100);
+	        }
 		
 		//B와 C가 같은 경우
-        else if(B == C) {
-        	System.out.print(1000 + B*100);
-        }
+	        else if(B == C) {
+	        	System.out.print(1000 + B*100);
+	        }
 		
 		//A와 B와 C가 모두 다른 경우
-        else {
-        	System.out.print((Math.max(Math.max(A, B), C)*100));	//Math.max함수로 최댓값 찾기
-        }
+	        else {
+	        	System.out.print((Math.max(Math.max(A, B), C)*100));	//Math.max함수로 최댓값 찾기
+	        }
     }
 }
