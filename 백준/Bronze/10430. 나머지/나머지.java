52cdf4d393d610ca28031d3c6672f84c78454e53
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-28 22:42:53 +0900

    Update 나머지.java

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		
 		//A, B, C 변수를 차례로 받는다(2 ≤ A, B, C ≤ 10000)
-		int A = scanner.nextInt();
-		int B = scanner.nextInt();
-		int C = scanner.nextInt();
-		
-		scanner.close();
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		int C = scan.nextInt();
+
+		//scanner 닫기
+		scan.close();
 	
 		//계산한 값 출력
 		System.out.println( (A+B)%C );
-		System.out.println( ((A%C) + (B%C))%C );
+		System.out.println( ((A%C) + (B%C)) %C );
 		System.out.println( (A*B)%C );
-		System.out.println( ((A%C)*(B%C))%C );
+		System.out.println( ((A%C)*(B%C)) %C );
 	}
 }
