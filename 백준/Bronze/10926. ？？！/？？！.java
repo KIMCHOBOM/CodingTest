0b784ad7934b7c7315adc86aaf851a21d510f90a
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-28 22:46:12 +0900

    Update ？？！.java

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		String A = scanner.nextLine();
+		Scanner scan = new Scanner(System.in);
+
+		// 문장 입력받기
+		String A = scan.nextLine();
 		String B = "??!";
-		
-		scanner.close();
-		
+
+		//scanner 닫기
+		scan.close();
+
+		// 입력 받은 문장에 ??! 추가하기 == (A+"??!")
 		String C = A.concat(B);
-		
+
+		//합친 문장 출력하기
 		System.out.println(C);
 	}
 }
