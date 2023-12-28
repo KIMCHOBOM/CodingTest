2b85d221acd3e881c64306879ea3054682a3ef0a
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-28 22:39:03 +0900

    Update A－B.java

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-      
-		int A = scanner.nextInt();
-		int B = scanner.nextInt();
-		
-		scanner.close();
-		
+		Scanner scan = new Scanner(System.in);
+
+		//A와 B값 입력받기
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+
+		//scanner 닫기
+		scan.close();
+
+		//A-B값 출력
 		System.out.println(A-B);
 	}
 }
