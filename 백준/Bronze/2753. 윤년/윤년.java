d71a1ac1d07fd599691a4f10a7e8fa014d8a75a0
KIMCHOBOM <149510771+KIMCHOBOM@users.noreply.github.com>
2023-12-28 22:15:32 +0900

    [Bronze V] Title: 윤년, Time: 112 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//년도를 입력 받는다!
+		int year = scan.nextInt();
+		
+		// 윤년O >> 1
+		if((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0)) {
+			System.out.println(1);
+		}
+		
+		// 윤년X >> 0
+		else {
+			System.out.println(0);
+		}
+	}
+}
